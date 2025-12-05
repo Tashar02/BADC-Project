76362cfe3f1af6ddda2bf662d94a6f9cca0e3c50
saadaintsad <mdsaadislam5206@gmail.com>
2025-12-05 23:16:54 +0600

    module abc

--- src/main/java/cse213/badc/saad/U3SupplierSignUpViewController.java
+++ src/main/java/cse213/badc/saad/U3SupplierSignUpViewController.java
@@ -1,7 +1,14 @@
 package cse213.badc.saad;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 
 public class U3SupplierSignUpViewController
 {
@@ -13,16 +20,37 @@ public class U3SupplierSignUpViewController
     private TextField idTF;
     @javafx.fxml.FXML
     private TextField mobileTF;
+    Supplier s;
 
     @javafx.fxml.FXML
     public void initialize() {
     }
 
     @javafx.fxml.FXML
-    public void dashboardOA(ActionEvent actionEvent) {
+    public void dashboardOA(ActionEvent actionEvent) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("saad/U3IESDashboardView.fxml"));
+
+        Scene scene = new Scene(loader.load());
+
+        U3IESDashboardController controller = loader.getController();
+        controller.passIESDashboard(s);
+
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+
+        stage.setTitle("IES Dashboard");
+        stage.setScene(scene);
+        stage.show();
+
     }
 
     @javafx.fxml.FXML
-    public void createOA(ActionEvent actionEvent) {
+    public void createOA(ActionEvent actionEvent) throws IOException {
+
+        // String supplierID, String name, String mobileNo, String email
+
+        s = new Supplier(idTF.getText(), nameTF.getText(), mobileTF.getText(), emailTF.getText());
+        Helper.writeInto("allSuppliers.bin", s);
+
     }
 }
