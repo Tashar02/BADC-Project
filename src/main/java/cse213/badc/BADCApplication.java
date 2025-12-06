f10c762716d5d8b8f52cc278669e33be32156017
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2025-12-06 15:33:57 +0600

    temp

--- src/main/java/cse213/badc/BADCApplication.java
+++ src/main/java/cse213/badc/BADCApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class BADCApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(BADCApplication.class.getResource("LoginView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(BADCApplication.class.getResource("rhythm/AdmitCardListView.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
